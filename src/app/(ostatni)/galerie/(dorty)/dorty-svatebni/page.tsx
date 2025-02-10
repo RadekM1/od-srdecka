6e2764ebd24d98c3154e3a2d1c3b353bb821f197
Radek Morong <radekmorong@email.cz>
2025-02-10 17:40:13 +0100

    gallery debug

--- src/app/(ostatni)/galerie/(dorty)/dorty-svatebni/page.tsx
+++ src/app/(ostatni)/galerie/(dorty)/dorty-svatebni/page.tsx
@@ -1,11 +1,7 @@
 import { cakesGallery } from "../../../../../../public/content/temporary-galleries";
 import ProductGallery from "@/components/UI/productGallery";
 
-
 const Page = () => {
-
-
-
   return (
     <>
       <ProductGallery productType="cake" gallery={cakesGallery} />
