fd3f7792d507d6eb52f2a5eaf60c10b8f8c393c2
Morong Radek <morra@ton.cz>
2025-02-17 17:46:05 +0100

    ISR first cache atempt

--- src/app/(ostatni)/blog/bezlepkove-recepty-ostatni/[slug]/page.tsx
+++ src/app/(ostatni)/blog/bezlepkove-recepty-ostatni/[slug]/page.tsx
@@ -1,12 +1,7 @@
-import Article from "@/components/article";
-import { articles } from "../../../../../../public/content/blog";
-
-const article = articles[0];
-
 const Page = () => {
   return (
     <div className="max-w-[1000px] w-full  text-center flex mx-3 px-2 justify-center items-center mb-10 flex-col">
-      {article ? <Article article={article} /> : "článek nenalezen"}
+      zatím prázdné
     </div>
   );
 };
