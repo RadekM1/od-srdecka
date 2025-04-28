fb6a64c4ae662cd96ba0d0d89eed18a9d35742e1
Morong Radek <morra@ton.cz>
2025-04-28 19:50:46 +0200

    feat: FINALLY ISR !!!

--- src/app/(ostatni)/[blog]/bezlepkove-recepty-ostatni/page.tsx
+++ src/app/(ostatni)/[blog]/bezlepkove-recepty-ostatni/page.tsx
@@ -1,42 +1,47 @@
 import BlogSections from "../blogSections";
-import SweetLayout from "./sweet-layout";
-import { wpFetchBlogArticlesSweets } from "@/lib/fetch/articles-sweets-fetch";
-
-export const dynamic = "force-dynamic";
+import OthersLayout from "./others-layout";
+import { wpFetchBlogArticlesOther } from "@/lib/fetch/articles-others-fetch";
 
 const blogSections = [
   {
     url: "/blog/bezlepkove-recepty-sladke",
     label: "Sladké recepty",
-    position: "left-[15%]  md:left-[20%]",
+    position: "left-[15%]  md:left-[20%] ",
   },
   {
     url: "/blog/bezlepkove-recepty-ostatni",
     label: "Ostatní recepty",
-    position: "right-[15%]  md:right-[20%]",
+    position: "right-[15%]  md:right-[20%] ",
   },
 ];
 
+export const revalidate = 10;
+export const dynamicParams = true;
+
+export async function generateStaticParams() {
+  return [{ blog: "bezlepkove-recepty-ostatni" }];
+}
+
 export default async function Page() {
-  const articleList = await wpFetchBlogArticlesSweets();
+  const articleList = await wpFetchBlogArticlesOther();
 
   return (
-    <SweetLayout articleList={articleList}>
+    <OthersLayout articleList={articleList}>
       <section className="flex mx-3 max-w-[1000px] px-2 justify-center items-center text-center mb-10 flex-col w-full">
         <div className="mb-6">
           <span className="text-3xl md:text-4xl font-semibold lg:text-5xl font-oldStandard w-full text-center">
             Bezlepkové recepty - ostatní
           </span>
         </div>
-        <div className="mt-20 mb-14 relative flex w-full justify-center items-center mx-4 max-w-[1000px]">
+        <div className="mt-20 mb-14 relative flex w-full  justify-center items-center mx-4 max-w-[1000px]">
           <img
-            className="absolute top-1/2 -translate-y-1/2 w-full"
+            className="absolute top-1/2  -translate-y-1/2 w-full"
             src="/others/line.svg"
             alt="oddělovací linka"
           />
           <BlogSections blogSections={blogSections} />
         </div>
       </section>
-    </SweetLayout>
+    </OthersLayout>
   );
 }
